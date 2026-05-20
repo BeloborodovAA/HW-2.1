203290cc4b17bb34c71cbb35845fa0ae08096496
Белобородова Алина <beloborodovaa574@gmail.com>
2026-05-20 08:54:50 +0700

    Бонусные мили исправленный

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int sum = 2327;
+        int sum = 23_068;
         int miles = sum / 20;
         System.out.println("Ваше количество миль: " + miles);
     }
+}
